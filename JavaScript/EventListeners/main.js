ceba6bb9e7aba5b8ab91be5bc595ac04744d4230
Srinivas Akella <akellasrinivas9944@gmail.com>
2024-06-13 21:10:07 +0530

    added form

--- JavaScript/EventListeners/main.js
+++ JavaScript/EventListeners/main.js
@@ -2,6 +2,11 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
+	const nameAnswer = document.getElementById("nameAnswer");
+	const hobbiesAnswer = document.getElementById("hobbiesAnswer");
+	const ageAnswer = document.getElementById("ageAnswer");
+	const genderAnswer = document.getElementById("genderAnswer");
+	const answers = document.getElementById("answers");
 
 	const hobbiesArray = [];
 	const name = document.getElementById("name").value;
@@ -15,4 +20,9 @@ form.addEventListener("submit", (event) => {
 	hobbiesArray.push("Selected Hobbies - " + selectedHobbies);
 
 	console.log(name, hobbiesArray.join("\n"), age, gender.value);
+	nameAnswer.textContent = `Name - ${name}`;
+	hobbiesAnswer.textContent = `Hobbies - ${hobbiesArray.join("\n")}`;
+	ageAnswer.textContent = `Age is between - ${age}`;
+	genderAnswer.textContent = `Gender - ${gender.value}`;
+	answers.classList.add("text-white");
 });
